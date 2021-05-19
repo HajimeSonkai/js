2efc0856790a0e494cff5d09dfc66bd1b45d86ff
sonkai <mfilho86@gmail.com>
2021-05-19 19:14:15 -0300

    programa 3 maior menor entre 3

--- scripts.js
+++ scripts.js
@@ -18,9 +18,23 @@ console.log("");
 let ap2 = 2
 let bp2 = 4
 
-
 if(ap2 > bp2) {
     console.log("o maior entre " + ap2 + " e " + bp2 + " é: " + ap2);
 } else {
     console.log("o maior entre " + ap2 + " e " + bp2 + " é: " + bp2);
+}
+
+console.log("");
+
+//progama 3
+let ap3 = 8;
+let bp3 = 12;
+let cp3 = 6;
+
+if (ap3 > bp3 && ap3 > cp3) {
+  console.log("o maior entre " + ap3 + " e " + bp3 + " e " + cp3 + " é: " + ap3);
+} else if (bp3 > ap3 && bp3 > cp3) {
+  console.log("o maior entre " + ap3 + " e " + bp3 + " e " + cp3 + " é: " + bp3);
+} else {
+    console.log("o maior entre " + ap3 + " e " + bp3 + " e " + cp3 + " é: " + cp3);
 }
