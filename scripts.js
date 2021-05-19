503f9ef6bae7c77238030fa75b920bf333b04925
sonkai <mfilho86@gmail.com>
2021-05-19 18:22:21 -0300

    repo de js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//programa 1
+let a = 2;
+let b = 2;
+
+console.log(a + "+" + b + "=")
+console.log(a + b);
+
+console.log(a + "-" + b + "=");
+console.log(a - b);
+
+console.log(a + "x" + b + "=");
+console.log(a * b);
+
+console.log(a + "/" + b + "=");
+console.log(a / b);
+
+console.log(a + "/" + b + " resta:");
+console.log(a % b);
