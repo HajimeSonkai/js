d7ee0c8420339ab97c72ee05d462884d83a3ea3f
sonkai <mfilho86@gmail.com>
2021-05-19 18:46:39 -0300

    programa 2 maior menor

--- scripts.js
+++ scripts.js
@@ -1,15 +1,26 @@
 //programa 1
-let a = 2;
-let b = 2;
+let ap1 = 2;
+let bp1 = 2;
 
-console.log(a + "+" + b + "=" + (a + b));
+console.log(ap1 + "+" + bp1 + "=" + (ap1 + bp1));
 
-console.log(a + "-" + b + "=" + (a - b));
+console.log(ap1 + "-" + bp1 + "=" + (ap1 - bp1));
 
-console.log(a + "x" + b + "=" + a * b);
+console.log(ap1 + "x" + bp1 + "=" + ap1 * bp1);
 
-console.log(a + "/" + b + "=" + a / b);
+console.log(ap1 + "/" + bp1 + "=" + ap1 / bp1);
 
-console.log(a + "/" + b + " resta:" + (a % b));
+console.log(ap1 + "/" + bp1 + " resta:" + (ap1 % bp1));
+
+console.log("");
 
 //programa 2
+let ap2 = 2
+let bp2 = 4
+
+
+if(ap2 > bp2) {
+    console.log("o maior entre " + ap2 + " e " + bp2 + " é: " + ap2);
+} else {
+    console.log("o maior entre " + ap2 + " e " + bp2 + " é: " + bp2);
+}
