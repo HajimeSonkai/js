6de5efdaf086ea2cd32b8c8a3020e616033b88d2
sonkai <mfilho86@gmail.com>
2021-05-19 19:53:10 -0300

    programa 5 triangulos

--- scripts.js
+++ scripts.js
@@ -52,3 +52,26 @@ if (ap4 >= 1) {
 } else {
   console.log("o numero " + ap4 + " é zero");
 }
+
+console.log("");
+
+// NOTE Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus. Um ângulo será considerado inválido se não tiver um valor positivo.
+
+let angulo1 = 90;
+let angulo2 = 80;
+let angulo3 = 10;
+let angulosSoma = (angulo1+angulo2+angulo3)
+
+if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0) {
+  switch (angulosSoma) {
+    case 180:
+      console.log("triangulo");
+      break;
+    default:
+      console.log("não triangulo");
+      break;
+  }
+} else {
+  console.log("angulos invalidos");
+}
+
