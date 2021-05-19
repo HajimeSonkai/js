7d230ef9e0ffca1dfa99ab80fc1fb2b4aaa4f124
sonkai <mfilho86@gmail.com>
2021-05-19 18:30:12 -0300

    programa 1 completo + formatacao

--- scripts.js
+++ scripts.js
@@ -2,17 +2,14 @@
 let a = 2;
 let b = 2;
 
-console.log(a + "+" + b + "=")
-console.log(a + b);
+console.log(a + "+" + b + "=" + (a + b));
 
-console.log(a + "-" + b + "=");
-console.log(a - b);
+console.log(a + "-" + b + "=" + (a - b));
 
-console.log(a + "x" + b + "=");
-console.log(a * b);
+console.log(a + "x" + b + "=" + a * b);
 
-console.log(a + "/" + b + "=");
-console.log(a / b);
+console.log(a + "/" + b + "=" + a / b);
 
-console.log(a + "/" + b + " resta:");
-console.log(a % b);
+console.log(a + "/" + b + " resta:" + (a % b));
+
+//programa 2
